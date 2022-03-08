418eae9ad3a7ddba136c07c6070dfeb5ecc81c49
Hùng Phạm <hungpm.works@gmail.com>
2022-03-08 10:06:33 +0700

    update

--- BlogLaravel/routes/web.php
+++ BlogLaravel/routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('CheckAuth')->group(function(){
     Route::get('/', function () {
-        return view('welcome');
+        return view('layouts.index');
     })->name('index');
 
 });
